51a59bb84db8b9f897b95d2e2b2c1ec28f8e3f17
gui <guixxguilherme1705@gmail.com>
2025-04-17 10:23:16 -0300

    concluido

--- src/app/pages/variaveis/detalhes/detalhes.page.ts
+++ src/app/pages/variaveis/detalhes/detalhes.page.ts
@@ -12,7 +12,31 @@ import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/stan
 })
 export class DetalhesPage implements OnInit {
 
-  constructor() { }
+  private route = inject (Router);
+  private activedRoute = injec(ActivatedRoute);
+  public nome!: String;
+  public idade!: Number;
+  public sexo!: String;
+  public s!: String;
+
+
+  constructor() { 
+    this.activedRoute.queryParams.subscribe (() => {
+      let state = this.route.getCurentNavigation ()?.extras.state ;
+      if (state) {
+        this.nome = state ['nome'];
+        this.idade = state ['idade'];
+        this.s = state ['sexo'];
+
+      }
+      if (this.s == 'm') {
+        this.sexo = 'Masculino';
+      } else {
+        this.sexo ='Feminino';
+      }
+
+    });
+  }
 
   ngOnInit() {
   }
